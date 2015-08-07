48142dca92da801b1615db1c5f65a39da3b68fdf
Anton Belousov <antonbelousov@iMac-Anton.local>
2015-08-07 22:37:22 +0300

    Initial Commit

---
+++ MatrixView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MatrixView
+//
+//  Created by Anton Belousov on 07/08/15.
+//  Copyright (c) 2015 novilab-mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MatrixView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MatrixView
+//
+//  Created by Anton Belousov on 07/08/15.
+//  Copyright (c) 2015 novilab-mobile. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
